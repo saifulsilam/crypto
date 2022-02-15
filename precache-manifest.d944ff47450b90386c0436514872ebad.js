5d63bbe80f500a06802f536bb2aa6ba86b7cccc1
Saiful Islam <saifulislam.ruet15@gmail.com>
2022-02-15 10:50:06 +0600

    Updates

---
+++ precache-manifest.d944ff47450b90386c0436514872ebad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152a3c9ec881af9cf869901639b5b3ab",
+    "url": "/github.io/crypto/index.html"
+  },
+  {
+    "revision": "30b1c4344545a2096c96",
+    "url": "/github.io/crypto/static/css/main.db8b421f.chunk.css"
+  },
+  {
+    "revision": "57033a26bf0b569ac284",
+    "url": "/github.io/crypto/static/js/2.c387dd5c.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/github.io/crypto/static/js/2.c387dd5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b1c4344545a2096c96",
+    "url": "/github.io/crypto/static/js/main.4fff8bf6.chunk.js"
+  },
+  {
+    "revision": "b2de7e9b550a8a511f32",
+    "url": "/github.io/crypto/static/js/runtime-main.8bf10762.js"
+  }
+]);
